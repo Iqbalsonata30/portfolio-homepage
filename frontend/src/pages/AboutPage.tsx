ad1ac74ab9ccf1a2fdcab575f6b05e845816ba88
iqbalsonata <iqbalsonata2@gmail.com>
2024-12-24 22:55:50 +0700

    refactor my personal website from jsx to tsx

--- frontend/src/pages/AboutPage.tsx
+++ frontend/src/pages/AboutPage.tsx
@@ -1,9 +1,15 @@
+import Description from "../components/Description";
+import FollowSection from "../components/FollowSection";
+import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 
 function AboutPage() {
   return (
     <>
       <Navbar />
+      <Description />
+      <FollowSection />
+      <Footer />
     </>
   );
 }
