52667477c74ca857e672871a27b1f0bb063f1b73
iqbalsonata <iqbalsonata2@gmail.com>
2025-05-09 13:24:51 +0700

    REFACTOR: refactoring home page

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,11 +1,19 @@
 import Layout from "@/components/layouts/Layout.tsx";
-import Navbar from "./../components/Navbar.tsx";
+import NavbarSection from "../components/NavbarSection.tsx";
+import PortfolioSection from "@/components/PortfolioSection.tsx";
+import FollowSection from "@/components/FollowSection.tsx";
+import HeroSection from "@/components/HeroSection.tsx";
+import FooterSection from "@/components/FooterSection.tsx";
 
 function HomePage() {
   return (
     <>
       <Layout>
-        <Navbar />
+        <NavbarSection />
+        <HeroSection />
+        <PortfolioSection />
+        <FollowSection />
+        <FooterSection />
       </Layout>
     </>
   );
